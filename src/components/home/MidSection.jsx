7531f1eba39fbae3326bbab4ad0830dffa2f0e14
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2024-08-31 09:53:33 +0530

    change the alt in midsection

--- src/components/home/MidSection.jsx
+++ src/components/home/MidSection.jsx
@@ -7,7 +7,7 @@ const MidSection = () => {
       <div className="flex flex-wrap justify-between mt-2">
         {imageURL.map((image, index) => (
           <div key={index} className="w-full lg:w-1/3 md:w-1/3 sm:w-full p-1">
-            <img src={image} alt={`image-${index}`} className="w-full h-auto" />
+            <img src={image} alt={index} className="w-full h-auto" />
           </div>
         ))}
       </div>
